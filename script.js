2d4901333d000b53f685102c5f1cf548b051c4fe
Elison <elisonluiz.com@gmail.com>
2025-03-26 09:13:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-document.addEventListener('DOMcoment')
+document.addEventListener('DOMcomentloaded', function(){
+const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+
+
+let tamanhoAtualFonte = 1; 
+
+aumentaFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize = '${tamanhoAtualFonte}rem'
+
+})
+
+diminuirFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fontSize = '${tamanhoAtualFonte}rem'
+
+})
+
+
+})
